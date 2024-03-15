03b50ea5ed53508f204721772fc9dd3527986619
eric2302 <ericgc95@gmail.com>
2024-03-15 11:20:08 -0400

    [ENH] added resampling and masking PET map to preprocessing

--- run_pvc.py
+++ run_pvc.py
@@ -1,4 +1,5 @@
 # %%
+# %%
 import argparse
 import os
 import warnings
@@ -14,14 +15,17 @@ def main(args):
 
     if os.path.exists(args.bids_dir):
         # not validated until derivatives structure is definded in BEP23
+        # not validated until derivatives structure is definded in BEP23
         layout = BIDSLayout(args.bids_dir, validate=False)
     else:
         raise Exception('BIDS directory does not exist')
     
     # get all PET files if no label is given
+    # get all PET files if no label is given
     if args.participant_label is None:
         args.participant_label = layout.get(suffix='pet', space='T1w', target='subject', return_type='id')
 
+    # create derivatives directories
     # create derivatives directories
     if args.output_dir is None:
         output_dir = os.path.join(args.bids_dir,'derivatives','petprep_pvc')
@@ -31,9 +35,11 @@ def main(args):
     os.makedirs(output_dir, exist_ok=True)
     
     # index all sessions and participants
+    # index all sessions and participants
     sessions = layout.get_sessions()
     participants = args.participant_label
     
+    # create prefix for filenames
     # create prefix for filenames
     if not sessions:
         file_prefix = [f'sub-{sub_id}' 
@@ -43,15 +49,27 @@ def main(args):
                        for sub_id, sess_id in zip(participants, sessions)]
 
     # whether to prepare anatomical data for PETPVC
+    # whether to prepare anatomical data for PETPVC
     if not args.skip_anat_prep:
+        # create 4D tissue segmentation (fourth dimension should add up to 1)
         # create 4D tissue segmentation (fourth dimension should add up to 1)
         for fp in file_prefix:
             if not sessions:
                 subj_dir = os.path.join(args.bids_dir, fp)
                 subj_out_dir = os.path.join(output_dir, fp)
+                subj_out_dir = os.path.join(output_dir, fp)
             else:
                 sub, ses = fp.split('_')
                 subj_dir = os.path.join(args.bids_dir, sub, ses)
+                subj_out_dir = os.path.join(output_dir, sub, ses)
+
+            os.makedirs(subj_out_dir, exist_ok=True)
+            
+            # find directory with anatomical data
+            anat_dir = find_anat_dir(args.bids_dir, subj_dir)
+            gm_prob = os.path.join(anat_dir, f'{fp}_label-GM_probseg.nii.gz')
+            wm_prob = os.path.join(anat_dir, f'{fp}_label-WM_probseg.nii.gz')
+            csf_prob = os.path.join(anat_dir, f'{fp}_label-CSF_probseg.nii.gz')
                 subj_out_dir = os.path.join(output_dir, sub, ses)
 
             os.makedirs(subj_out_dir, exist_ok=True)
@@ -70,11 +88,14 @@ def main(args):
                 raise Exception('Check that {GM,WM,CSF}_probseg exists in anat directory: ', anat_dir)
             
             prepare_anat(fp, gm_prob, wm_prob, csf_prob, subj_out_dir)
-
+            
+            
     # run PETPVC for every session and participant included in the analysis
     for fp in file_prefix:
         if not sessions:
             subj_dir = os.path.join(args.bids_dir, fp)
+            subj_out_dir = os.path.join(output_dir, fp)
+            pet_dir = os.path.join(args.bids_dir, 'derivatives', 'petprep_extract_tacs', fp)
             subj_out_dir = os.path.join(output_dir, fp)
             pet_dir = os.path.join(args.bids_dir, 'derivatives', 'petprep_extract_tacs', fp)
         else:
@@ -82,43 +103,31 @@ def main(args):
             subj_dir = os.path.join(args.bids_dir, sub, ses)
             subj_out_dir = os.path.join(output_dir, sub, ses)
             pet_dir = os.path.join(args.bids_dir, 'derivatives', 'petprep_extract_tacs', sub, ses)
-            
-        seg_fn = f'{fp}_desc-4Danatseg.nii.gz'
-        if not os.path.exists(os.path.join(subj_out_dir, seg_fn)):
-            raise Exception(f"No tissue segmentation in output folder for {fp}. "
-                            f"Make sure it is named '{fp}_desc-4Danatseg.nii.gz'")
+        
+        anat_dir = find_anat_dir(args.bids_dir, subj_dir)
+        t1 = os.path.join(anat_dir, f'{fp}_desc-preproc_T1w.nii.gz')
+        brain_mask = os.path.join(anat_dir, f'{fp}_desc-brain_mask.nii.gz')
         
         subj_out_dir = os.path.abspath(subj_out_dir)
         pet_dir = os.path.abspath(pet_dir)
         pet_fn = f'{fp}_space-T1w_desc-twa_pet.nii.gz'
         
-        align_seg_to_pet(subj_out_dir, seg_fn, pet_dir, pet_fn)
-                       
-        # pet_fn = os.path.basename(layout.get(suffix='pet',
-        #                                      space = 'T1w', 
-        #                                      subject=sub.split('-')[1], 
-        #                                      extension=['.nii', '.nii.gz'], 
-        #                                      return_type='filename')[0])
+        resample_pet_to_native(pet_dir, pet_fn, subj_out_dir, t1)
+        mask_pet(pet_fn, brain_mask, subj_out_dir)
+        
+        seg_fn = f'{fp}_desc-4Danatseg.nii.gz'
+        if not os.path.exists(os.path.join(subj_out_dir, seg_fn)):
+            raise Exception(f"No tissue segmentation in output folder for {fp}. "
+                            f"Make sure it is named '{fp}_desc-4Danatseg.nii.gz'")                       
+
         method = args.pvc_method.lower()
         pet_pvc_fn = f'{fp}_space-T1w_pvc-{method}_desc-preproc_pet.nii.gz'
         cmd = ("petpvc "
-               f"-i {pet_dir}/{pet_fn} "
+               f"-i {subj_out_dir}/{pet_fn} "
                f"-m {subj_out_dir}/{seg_fn} "
                f"-o {subj_out_dir}/{pet_pvc_fn} "
                f"-p {args.pvc_method} "
                f"-x {args.fwhm} -y {args.fwhm} -z {args.fwhm}")
-
-
-        # cmd = ("docker run "
-        #        f"-v {pet_dir}:/input "
-        #        f"-v {subj_out_dir}:/output "
-        #        f"{args.version}/petpvc petpvc "
-        #        f"-i /input/{pet_fn} "
-        #        f"-m /output/{seg_fn} "
-        #        f"-o /output/{pet_pvc_fn} "
-        #        f"-p {args.pvc_method} "
-        #        f"-x {args.fwhm} -y {args.fwhm} -z {args.fwhm}"
-        #         )
         
         print("Running PETPVC")
         result = subprocess.run(cmd, shell=True, text=True, 
@@ -141,8 +150,26 @@ def find_anat_dir(bids_dir, subj_dir):
         anat_dir = os.path.join(subj_dir, 'anat')
         return anat_dir
 
+# HELPER FUNCTIONS
+
+def find_anat_dir(bids_dir, subj_dir):
+    subj_path = os.path.relpath(subj_dir, bids_dir)
+
+    # check if smriprep directory exists in the derivatives folder
+    smriprep_dir = os.path.join(bids_dir, 'derivatives', 'smriprep', subj_path)
+
+    if os.path.exists(smriprep_dir):
+        # return anat directory in the smriprep folder
+        anat_dir = os.path.join(bids_dir, 'derivatives', 'smriprep', subj_path, 'anat')
+        return anat_dir
+    else:
+        # return anat directory in the subject folder
+        anat_dir = os.path.join(subj_dir, 'anat')
+        return anat_dir
+
 def prepare_anat(sub, gm_prob, wm_prob, csf_prob, output_dir):
     print(f"Preparing segmentation for {sub}")
+    # dividing by zero - turning off warnings temporarily
     # dividing by zero - turning off warnings temporarily
     warnings.filterwarnings("ignore", category=RuntimeWarning)  
                 
@@ -168,19 +195,34 @@ def prepare_anat(sub, gm_prob, wm_prob, csf_prob, output_dir):
     norm_csf = ni.new_img_like(csf_prob, data)
 
     seg_fn = os.path.join(output_dir, f'{sub}_desc-4Danatseg.nii.gz')
+    seg_fn = os.path.join(output_dir, f'{sub}_desc-4Danatseg.nii.gz')
     seg = ni.concat_imgs([norm_gm, norm_wm, norm_csf])
     seg.to_filename(seg_fn)
 
+    # reset warnings
     # reset warnings
     warnings.resetwarnings()
     
     return None
 
-def align_seg_to_pet(seg_dir, seg_fn, pet_dir, pet_fn):
-    seg_img = nib.load(os.path.join(seg_dir, seg_fn))
-    pet_img = nib.load(os.path.join(pet_dir, pet_fn))
-    seg_img = ni.resample_to_img(seg_img, pet_img, interpolation='nearest')
-    seg_img.to_filename(os.path.join(seg_dir, seg_fn))
+def resample_pet_to_native(pet_dir, pet_fn, subj_out_dir, t1):
+    pet = os.path.join(pet_dir, pet_fn)
+    
+    pet_rs = ni.resample_to_img(pet, t1, interpolation='continuous', clip=True)
+    pet_rs = ni.new_img_like(t1, pet_rs.get_fdata())
+    pet_rs.to_filename(os.path.join(subj_out_dir, pet_fn))
+    
+    return None
+
+def mask_pet(pet_fn, brain_mask, subj_out_dir):
+    pet_fn = os.path.join(subj_out_dir, pet_fn)
+    mask = ni.math_img("img > 0", 
+                       img=brain_mask)
+    
+    pet_masked = ni.math_img("img1 * img2", 
+                             img1=pet_fn, 
+                             img2=mask)
+    pet_masked.to_filename(pet_fn)
     
     return None
 
